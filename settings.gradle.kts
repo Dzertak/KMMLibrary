f00bea92da2a240a9ae6a99ffeecddc1c5279892
Dzertak <kraftchenko.io@gmail.com>
2024-01-26 16:51:48 +0200

    init kmm library project

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    versionCatalogs {
+        create("commonlibs") {
+            from(files("gradle/libs.versions.toml"))
+        }
+    }
+}
+
+rootProject.name = "KMMLibray"
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/dzertak/kmmlibray/Platform.kt
@@ -0,0 +1,7 @@
+package com.dzertak.kmmlibray
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
